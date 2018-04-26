230268f6a19a8f69d74574b8a31aee19a3b0886c
Mosef Asad <mosefasad@gmail.com>
2018-04-26 18:30:27 -0400

    finished initial project set-up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Lab</h1>
         </header>
       </div>
     );
